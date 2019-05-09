9c8dfa7443771f299e1770d18130d0c101f38293
Xavier MARCELET <xavier@marcelet.com>
2019-05-09 17:56:43 +0200

    wip

---
+++ docker/install.sh
@@ -0,0 +1,5 @@
+which docker-compose || {
+  curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+}
+
