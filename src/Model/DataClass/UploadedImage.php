1df970b22b1e8e315524fb929dc7457a5db2c50d
Sylvain Dangin <sylvain.dangin@gmail.com>
2020-01-06 16:31:55 +0100

    Fix du problème d'image sur les designs
    https://github.com/NextDom/nextdom-core/issues/1565
    https://github.com/NextDom/nextdom-core/issues/1566
    
    Tester la suppression et l'ajout d'une image JPG dans les designs

---
+++ src/Model/DataClass/UploadedImage.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace NextDom\Model\DataClass;
+
+
+class UploadedImage
+{
+    private $type;
+    private $hash;
+    private $size;
+    private $data;
+    private $path;
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHash()
+    {
+        return $this->hash;
+    }
+
+    /**
+     * @param mixed $hash
+     */
+    public function setHash($hash): void
+    {
+        $this->hash = $hash;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * @param mixed $size
+     */
+    public function setSize($size): void
+    {
+        $this->size = $size;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSizeX()
+    {
+        return $this->size[0];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSizeY()
+    {
+        return $this->size[1];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param mixed $data
+     */
+    public function setData($data): void
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param mixed $path
+     */
+    public function setPath($path): void
+    {
+        $this->path = $path;
+    }
+}
